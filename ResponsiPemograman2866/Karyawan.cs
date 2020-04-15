a29bba08e233743df4ee8eaeea160d16545ad061
GyanEkasatria <gyan.ekasatria@students.amikom.ac.id>
2020-04-15 15:25:53 +0700

    Responsi

---
+++ ResponsiPemograman2866/Karyawan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemograman2866
+{
+    public class Karyawan
+    {
+        string Name;
+        string Nik;
+        string GajiBulanan;
+        public Karyawan(string name, string nik, string gaji)
+        {
+            Name = name;
+            Nik = nik;
+            GajiBulanan = gaji;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Pertama");
+            Console.WriteLine("Nama : {0} ", Name);
+            Console.WriteLine("Nik  : {0}", Nik);
+            Console.WriteLine("Gaji Bulanan  : {0}", GajiBulanan);
+            Console.WriteLine("------------------------------");
+        }
+        public void GetInfo1()
+        {
+            Console.WriteLine("Kedua");
+            Console.WriteLine("Nama : {0} ", Name);
+            Console.WriteLine("Nik  : {0}", Nik);
+            Console.WriteLine("Gaji Bulanan  : {0}", GajiBulanan);
+            Console.WriteLine("------------------------------");
+        }
+    }
+}
+
---
+++ ResponsiPemograman2866/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ResponsiPemograman2866
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan("Grizzly", "16253", "3000000");
+            karyawan.GetInfo();
+
+            Karyawan karyawan1 = new Karyawan("Panda", "16253", "3300000");
+            karyawan1.GetInfo1();
+        }
+    }
+}
